43936679ecf6f1af84cbb05205fd124aedd9fbc1
HabibMollah <hrmollah2002@gmail.com>
2023-05-13 16:39:48 +0600

    add state

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import { useForm } from 'react-hook-form';
 import Form from './Form';
 import ExpenseList from './ExpenseList';
+import { useState } from 'react';
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: 'Milk', amount: '5', category: 'Groceries' },
+    { id: 2, description: 'Water', amount: '1', category: 'Groceries' },
+    { id: 3, description: 'Radio', amount: '15', category: 'Entertainment' },
+    { id: 4, description: 'Electric wire', amount: '3', category: 'Utilities' },
+    { id: 5, description: 'TV', amount: '500', category: 'Entertainment' },
+    { id: 6, description: 'Rice', amount: '8', category: 'Groceries' },
+  ]);
   const { register, handleSubmit } = useForm();
   return (
     <div>
